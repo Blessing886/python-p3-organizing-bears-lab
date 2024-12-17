4df28c6d389a5ac491c69b7914480e87d854646a
BlessingOkora <moraablessing082@gmail.com>
2024-12-17 07:14:44 +0300

    performs crud operatios

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color,temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', TRUE),
+('Rowdy', 7, 'M', 'black', 'playful', TRUE),
+('Tabitha', 3, 'F', 'white', 'gentle', TRUE),
+('Sergeant Brown', 12, 'M', 'brown', 'aggressive', FALSE),
+('Melissa', 5, 'F', 'black', 'friendly', TRUE),
+('Grinch', 6, 'M', 'gray', 'grumpy', FALSE),
+('Wendy', 8, 'F', 'blonde', 'curious', TRUE),
+(NULL, NULL, NULL, NULL, NULL, NULL);  
